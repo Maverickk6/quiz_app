508eb129756ba06b096c6e77b05e373db0b869be
System Administrator <eddymav247@gmail.com>
2020-05-08 10:53:42 +0100

    improved summary visual

--- lib/quiz_questions.dart
+++ lib/quiz_questions.dart
@@ -288,29 +288,33 @@ class Summary extends StatelessWidget {
   Widget build(BuildContext context) {
     return new WillPopScope(
       onWillPop: () async => false,
-      child: Scaffold(
-        body: new Container(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(
-                "Final Score: $score",
-                style: new TextStyle(fontSize: 35.0),
+      child: Center(
+        child: Scaffold(
+          body: Center(
+            child: new Container(
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text(
+                    "Final Score: $score",
+                    style: new TextStyle(fontSize: 35.0),
+                  ),
+                  new Padding(padding: EdgeInsets.all(30.0)),
+                  new MaterialButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      questionNumber = 0;
+                      finalScore = 0;
+                      Navigator.pop(context);
+                    },
+                    child: new Text(
+                      "Reset Quiz",
+                      style: new TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
+                  )
+                ],
               ),
-              new Padding(padding: EdgeInsets.all(30.0)),
-              new MaterialButton(
-                color: Colors.red,
-                onPressed: () {
-                  questionNumber = 0;
-                  finalScore = 0;
-                  Navigator.pop(context);
-                },
-                child: new Text(
-                  "Reset Quiz",
-                  style: new TextStyle(fontSize: 20.0, color: Colors.white),
-                ),
-              )
-            ],
+            ),
           ),
         ),
       ),
